d95dbdf09d164962bddef6b3e40f9222c694105b
federicamato00 <134133171+federicamato00@users.noreply.github.com>
2024-02-13 12:17:29 +0100

    training_loss fix

--- train/main.py
+++ train/main.py
@@ -35,12 +35,12 @@ color_transform = Colorize(NUM_CLASSES)
 image_transform = ToPILImage()
 
 class FocalLoss(torch.nn.Module):
-    def __init__(self, gamma=2.0, alpha=None, size_average=True):
+    def __init__(self, gamma=2.0, alpha=1, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha,(float,int,torch.long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        # if isinstance(alpha,(float,int,torch.long)): self.alpha = torch.Tensor([alpha,1-alpha])
+        # if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
         self.size_average = size_average
 
     def forward(self, input, target):
@@ -55,11 +55,11 @@ class FocalLoss(torch.nn.Module):
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
 
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+        # if self.alpha is not None:
+        #     if self.alpha.type()!=input.data.type():
+        #         self.alpha = self.alpha.type_as(input.data)
+        #     at = self.alpha.gather(0,target.data.view(-1))
+        #     logpt = logpt * Variable(at)
 
         loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average: return loss.mean()
@@ -189,7 +189,9 @@ def train(args, model, enc=False):
         weights = 1 / (label_counts / total_samples)
 
         return weights
-
+    
+    
+        
     weight = torch.ones(NUM_CLASSES)
     if (enc):
         weight[0] = 2.3653597831726	
@@ -233,7 +235,7 @@ def train(args, model, enc=False):
         weight[18] = 10.138095855713	
 
     weight[19] = 0
-
+    
     assert os.path.exists(args.datadir), "Error: datadir (dataset directory) could not be loaded"
     print("transform")
     co_transform = MyCoTransform(enc, augment=True, height=args.height)#1024)
